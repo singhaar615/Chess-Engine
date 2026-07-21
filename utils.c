bc83ee075db08ba9c70803006a60a2c442c7f855
Aaruni Singh <singhaar615@gmail.com>
2026-07-20 21:17:30 -0400

    Update utils.c

--- utils.c
+++ utils.c
@@ -1,5 +1,4 @@
-// DO NOT MODIFY
-// PROVIDES FUNCTIONS TO SEND AND RECEIVE INPUTS FROM AUTOGRADER
+//PROVIDES FUNCTIONS TO SEND AND RECEIVE INPUTS FROM PREDEFINED PROGRAM
 #include "chess.h"
 
 int get_color()
@@ -13,12 +12,12 @@ int get_color()
 int get_move(move_t move, int *time_me, int *time_opp)
 {
     if (scanf(" %c%c%c%c%c %d %d", &move[0], &move[1], &move[2], &move[3], &move[4], time_me, time_opp) != 7)
-        return -1; // Should never happen
+        return -1; //should never happen
     return 0;
 }
 
 void send_move(const move_t move)
 {
     printf("%c%c%c%c%c\n", move[0], move[1], move[2], move[3], move[4]);
-    fflush(stdout); // Very important otherwise your engine times out because of buffering
+    fflush(stdout); //very important otherwise your engine times out because of buffering
 }
